cd6b871bedfcfb5289c304506acb725aeeabae6d
Robin hawlader <robinhawlader360@gmail.com>
2022-03-31 10:02:00 +0600

    Edit

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -9,28 +9,28 @@ const About = () => {
   return (
     <Container style={{ marginTop: "150px" }}>
       <div>
-        <Row md={3} xs={1} sm={1} className="mb-3">
+        <Row md={3} xs={1} sm={1}>
           <Col>
-            <img className="img-fluid" src={shwapon} alt="" />
+            <img className="img-fluid" src={sagar} alt="" />
           </Col>
           <Col md={8} className="text-start author">
             <div>
               <h1 className="text-uppercase name-prop">
                 <span className="border-bottom border-4 border-danger">
-                  Burhanuddin
+                  sagar
                 </span>
-                -Ahmed
+                -khalashi
               </h1>
               <p className="text-uppercase text-property fs-4 mt-5">
-                Burhanuddin Ahmed is acting as the chief advisor of Mujraikandi
-                Youth Welfare Association. In 2020, the organization underwent a
-                major overhaul. He is currently the founder and director of the
-                National Trading Corporation. Through his efforts he has formed
-                the Abdul Gafur Mia Foundation.
+                Sagar Khalasi is currently serving as the President of
+                Muzraikandi Youth Welfare Association. In 2019, he was convened
+                and a full 42-member committee was announced. But later in 2020,
+                he was elected president by the rest of the organization. He is
+                currently performing his duties with utmost devotion.
               </p>
               <p className="fs-2 mt-4">
                 <a
-                  href="https://www.facebook.com/burhanuddin.ahmed.3956"
+                  href="https://www.facebook.com/sagar.ahmed.1238"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -61,28 +61,28 @@ const About = () => {
  Burhanuddin profile end
  Sagar-khalashi profile start
 -------------------------------------------------------------------------- */}
-        <Row md={3} xs={1} sm={1}>
+        <Row md={3} xs={1} sm={1} className="mb-3">
           <Col>
-            <img className="img-fluid" src={sagar} alt="" />
+            <img className="img-fluid" src={shwapon} alt="" />
           </Col>
           <Col md={8} className="text-start author">
             <div>
               <h1 className="text-uppercase name-prop">
                 <span className="border-bottom border-4 border-danger">
-                  sagar
+                  Burhanuddin
                 </span>
-                -khalashi
+                -Ahmed
               </h1>
               <p className="text-uppercase text-property fs-4 mt-5">
-                Sagar Khalasi is currently serving as the President of
-                Muzraikandi Youth Welfare Association. In 2019, he was convened
-                and a full 42-member committee was announced. But later in 2020,
-                he was elected president by the rest of the organization. He is
-                currently performing his duties with utmost devotion.
+                Burhanuddin Ahmed is acting as the chief advisor of Mujraikandi
+                Youth Welfare Association. In 2020, the organization underwent a
+                major overhaul. He is currently the founder and director of the
+                National Trading Corporation. Through his efforts he has formed
+                the Abdul Gafur Mia Foundation.
               </p>
               <p className="fs-2 mt-4">
                 <a
-                  href="https://www.facebook.com/sagar.ahmed.1238"
+                  href="https://www.facebook.com/burhanuddin.ahmed.3956"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
